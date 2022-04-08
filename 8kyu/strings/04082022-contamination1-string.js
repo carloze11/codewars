99fb684cf969720ae3ebc71cd732d0079f4c4414
Carlos Estrada <cestrada1109@gmail.com>
2022-04-08 13:44:30 -0500

    Daily coding challenge

---
+++ 8kyu/strings/04082022-contamination1-string.js
@@ -0,0 +1,14 @@
+// An AI has infected a text with a character!!
+
+// This text is now fully mutated to this character.
+
+// If the text or the character are empty, return an empty string.
+// There will never be a case when both are empty as nothing is going on!!
+
+// Note: The character is a string of length 1 or an empty string.
+
+function contamination(text, char){
+    if (typeof char === 'string' ) {
+      return text.split('').map(x => char).join('')
+    }
+  }
