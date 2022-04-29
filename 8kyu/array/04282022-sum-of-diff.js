a57b776724b6972b1683df74df8a5087b6bf9e16
Carlos Estrada <cestrada1109@gmail.com>
2022-04-28 23:02:33 -0500

    forgot to enter answer lol

--- 8kyu/array/04282022-sum-of-diff.js
+++ 8kyu/array/04282022-sum-of-diff.js
@@ -12,3 +12,16 @@
 // Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
 
 // If the array is empty or the array has only one element the result should be 0 (Nothing in Haskell).
+
+function sumOfDifferences(arr) {
+    let sum = 0
+    arr.sort((a, b) => b - a)
+    if (arr.length <= 1) {
+      return 0
+    } else {
+      for (let i = 0; i < arr.length-1; i++){
+        sum += (arr[i] - arr[i+1])
+      }
+    }
+    return sum
+  }
