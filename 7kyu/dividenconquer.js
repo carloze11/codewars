3fea6faf64bfd8428a82030734442ca1f0288af1
Carlos Estrada <cestrada1109@gmail.com>
2022-06-05 23:55:28 -0500

    get BINGO

--- 7kyu/dividenconquer.js
+++ 7kyu/dividenconquer.js
@@ -1,6 +1,6 @@
-Given a mixed array of number and string representations of integers, add up the string integers and subtract this from the total of the non-string integers.
+// Given a mixed array of number and string representations of integers, add up the string integers and subtract this from the total of the non-string integers.
 
-Return as a number.
+// Return as a number.
 
 
 function divCon(x){
