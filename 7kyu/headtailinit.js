1c91ab1479be1a83b6842f1cd81b59b5d2860d26
Carlos Estrada <cestrada1109@gmail.com>
2022-06-09 22:25:02 -0500

    follow orders

---
+++ 7kyu/headtailinit.js
@@ -0,0 +1,5 @@
+// TODO: implement the four functions specified.
+let head = arr => arr[0]
+let tail = arr => arr.slice(1)
+let init = arr => arr.slice(0, arr.length - 1)
+let last = arr => arr[arr.length-1]
