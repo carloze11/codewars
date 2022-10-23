1b212ec8ff8aed2427ff82fae7c5944f9d7216cb
Carlos Estrada <cestrada1109@gmail.com>
2022-10-22 23:41:52 -0500

    Daily coding challenge

---
+++ leetcode/2sum.js
@@ -0,0 +1,11 @@
+var twoSum = function(nums, target) {
+    let output = []
+    for (let i = 0; i < nums.length; i++){
+        for (let j = 0; j < nums.length; j++){
+            if (nums[i] + nums[j] === target && i !== j){
+                output.push(i)
+            }
+        }
+    }
+    return output
+};
