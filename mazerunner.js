8c77c7b9d66cd27a8ec6784429ba08e0ace4961e
Carlos Estrada <cestrada1109@gmail.com>
2022-11-27 23:49:40 -0600

    Daily coding challenge

---
+++ mazerunner.js
@@ -0,0 +1,26 @@
+function mazeRunner(maze, directions) {
+    var path = Array(),
+        curX,
+        curY;
+    for (var i = 0; i < maze.length; i++) {
+        for (var j = 0; j < maze[i].length; j++) {
+            if (maze[i][j] == 2) {
+                curX = j;
+                curY = i;
+            }
+        }
+    }
+    for (var i = 0; i < directions.length; i++) {
+        if (directions[i] == "N") curY--;
+        if (directions[i] == "S") curY++;
+        if (directions[i] == "W") curX--;
+        if (directions[i] == "E") curX++;
+
+        // path.push(maze[curY][curX]);
+        if (!maze[curY]) return "Dead";
+        if (maze[curY][curX] == 3) return "Finish";
+        if (maze[curY][curX] == undefined || maze[curY][curX] == 1)
+            return "Dead";
+    }
+    return "Lost";
+}
