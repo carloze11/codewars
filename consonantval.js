e97a44020044510e18ee13f3a01a8f713f5cfde9
Carlos Estrada <cestrada1109@gmail.com>
2022-12-10 22:16:39 -0600

    Add instr

--- consonantval.js
+++ consonantval.js
@@ -1,3 +1,11 @@
+// Given a lowercase string that has alphabetic characters only and no spaces, return the highest value of consonant substrings. Consonants are any letters of the alphabet except "aeiou".
+
+// We shall assign the following values: a = 1, b = 2, c = 3, .... z = 26.
+
+// For example, for the word "zodiacs", let's cross out the vowels. We get: "z o d ia cs"
+
+//Brute force. In hindsight, I should have created a function to check for consonant and another to return char val.
+
 function solve(s) {
     let alph = {
         a: 1,
