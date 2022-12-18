db9c94f4d196127dc82c8891846429b689088f69
Carlos Estrada <cestrada1109@gmail.com>
2022-12-17 22:31:00 -0600

    Daily coding challenge

---
+++ arrdeepcount.js
@@ -0,0 +1,26 @@
+// You are given an array. Complete the function that returns the number of ALL elements within an array, including any nested arrays.
+
+// Examples
+// []                   -->  0
+// [1, 2, 3]            -->  3
+// ["x", "y", ["z"]]    -->  4
+// [1, 2, [3, 4, [5]]]  -->  7
+// The input will always be an array.
+
+function deepCount(a) {
+    let count = 0;
+
+    function recurse(arr) {
+        arr.forEach((el) => {
+            if (!Array.isArray(el)) {
+                return count++;
+            } else {
+                count++;
+                return recurse(el);
+            }
+        });
+    }
+    recurse(a);
+
+    return count;
+}
