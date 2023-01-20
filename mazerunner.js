48d69c3703127ace86da0daeb739b9bcd696a1e5
Carlos Estrada <cestrada1109@gmail.com>
2023-01-19 22:23:57 -0600

    Daily coding challenge

--- mazerunner.js
+++ mazerunner.js
@@ -1,26 +1,45 @@
+// You will be given a 2D array of the maze and an array of directions. Your task is to follow the directions given. If you reach the end point before all your moves have gone, you should return Finish. If you hit any walls or go outside the maze border, you should return Dead. If you find yourself still in the maze after using all the moves, you should return Lost.
+
 function mazeRunner(maze, directions) {
-    var path = Array(),
-        curX,
-        curY;
-    for (var i = 0; i < maze.length; i++) {
-        for (var j = 0; j < maze[i].length; j++) {
-            if (maze[i][j] == 2) {
-                curX = j;
-                curY = i;
+    // get current position
+    let currentPos = [];
+    maze.forEach((arr, i) => {
+        if (arr.includes(2)) {
+            currentPos.push(i);
+            currentPos.push(arr.indexOf(2));
+        }
+    });
+
+    let result = "Lost";
+
+    // change current position based on direction given
+    directions.forEach((dir) => {
+        switch (dir) {
+            case "N":
+                currentPos[0] -= 1;
+                break;
+            case "S":
+                currentPos[0] += 1;
+                break;
+            case "W":
+                currentPos[1] -= 1;
+                break;
+            case "E":
+                currentPos[1] += 1;
+                break;
+        }
+        if (result === "Lost") {
+            if (
+                maze[currentPos[0]] === undefined ||
+                maze[currentPos[0]][currentPos[1]] === undefined
+            ) {
+                result = "Dead";
+            } else if (maze[currentPos[0]][currentPos[1]] === 1) {
+                result = "Dead";
+            } else if (maze[currentPos[0]][currentPos[1]] === 3) {
+                result = "Finish";
             }
         }
-    }
-    for (var i = 0; i < directions.length; i++) {
-        if (directions[i] == "N") curY--;
-        if (directions[i] == "S") curY++;
-        if (directions[i] == "W") curX--;
-        if (directions[i] == "E") curX++;
-
-        // path.push(maze[curY][curX]);
-        if (!maze[curY]) return "Dead";
-        if (maze[curY][curX] == 3) return "Finish";
-        if (maze[curY][curX] == undefined || maze[curY][curX] == 1)
-            return "Dead";
-    }
-    return "Lost";
+    });
+    return result;
 }
